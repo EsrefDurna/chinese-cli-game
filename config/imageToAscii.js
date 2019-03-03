47d72487b8d67b358de8f91fd987ae8a57d662b3
Ash Durna <esref.durna@regiscorp.com>
2019-03-03 12:39:33 -0800

    use dotenv, eslint

---
+++ config/imageToAscii.js
@@ -0,0 +1,18 @@
+module.exports = {
+    size_options: {
+        px_size: {
+            width: 1
+        }
+    },
+    concat: true,
+    bg: true,
+    fg: false,
+    stringify: false,
+    size: {
+        height: "100%"
+    },
+    pixels: "            ",
+    white_bg: true
+    //pixels: " .,:;i1tfL..0##"
+    //stringify_fn: asciifyPixelMatrix
+};
--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-// Dependencies
-const imageToAscii = require("image-to-ascii");
+require('dotenv').config();
+const imageToAscii = require('image-to-ascii');
+const textToImage = require('text-to-image');
+const base64Img = require('base64-img');
+const DEFAULTS = require('./config/imageToAscii');
+const textToImageConfig = require('./config/textToImage');
 
-var textToImage = require('text-to-image');
-var base64Img = require('base64-img');
-var charWidth = require("terminal-char-width");
-
-var DEFAULTS = {
-    size_options: {
-        px_size: {
-            width: 1
-        }
-    },
-    concat: true,
-    bg: true,
-    fg: false,
-    stringify: false,
-    size: {
-        height: "100%"
-    },
-    pixels: "            ",
-    white_bg: true
-    //pixels: " .,:;i1tfL..0##"
-    //stringify_fn: asciifyPixelMatrix
-};
-function display(err,data) {
-    if (!err) {
-        imageToAscii(data, DEFAULTS, (err, converted) => {
-            console.log(err || converted);
-        });
-    }
+function display(err, data) {
+  if (!err) {
+    imageToAscii(data, DEFAULTS, (error, converted) => {
+      console.log(error || converted);
+    });
+  }
 }
-let name = '百度推'
-let factor = 10;
-let defaults = {
-    debug: false,
-    maxWidth: 200*factor,
-    fontSize: 60*factor,
-    lineHeight: 180*factor,
-    margin: 10,
-    bgColor: '#000',
-    textColor: '#003300',
-	fontFamily: 'Helvetica'
-  };
-  
-let destpath = './';
-textToImage.generate(name,defaults).then(function (dataUri) {
-    base64Img.img(dataUri, destpath, name, display)
+const name = '百度推';
+const tempPath = process.env.tempPath || 'tmp';
+textToImage.generate(name, textToImageConfig).then((dataUri) => {
+  base64Img.img(dataUri, tempPath, name, display);
 });
-
-console.log('百度推')
+console.log('百度推');
