dba9426666fd18deed1c95558a8b38b1c4e6ec61
Ash Durna <esref.durna@regiscorp.com>
2019-03-03 12:39:33 -0800

    use dotenv, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",    
+    "rules":{
+      //"no-trailing-spaces": ["error", { "ignoreComments": true }],
+      //"no-multiple-empty-lines": ["error", {"max": 0}],
+      //"import/newline-after-import": ["off", { "count": 1 }],
+      //"prefer-rest-params": ["off"]
+    }
+  };
---
+++ config/textToImage.js
@@ -0,0 +1,13 @@
+const factor = 1;
+module.exports = {
+    debug: false,
+    maxWidth: 200*factor,
+    fontSize: 60*factor,
+    lineHeight: 180*factor,
+    margin: 10,
+    bgColor: '#000',
+    textColor: '#003300',
+	fontFamily: 'Helvetica'
+};
+
+
